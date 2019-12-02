9ccda7412e4d7aaea3c1682cc7f9591b7584cb8d
yangzhan <yzlz5259@163.com>
2019-12-02 15:59:43 +0800

    添加注释文字；

--- input.js
+++ input.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 fs.readFile('input.txt', function (err, data) {
   if (err) return console.error(err); // 错误信息输出
-  console.log(data.toString()); // 回调函数
+  console.log(data.toString()); // 回调函数 对读取的数据进行序列化，因为data 是二进制流形式
 });
 console.log(__filename, __dirname)
 console.log('----程序执行结束----')
